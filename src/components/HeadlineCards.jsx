30d16ac9148ad42cb0a24f4c5b539309ac1e9788
mengot <mengotnso@gmail.com>
2023-12-13 03:15:40 +0100

    my first commit

---
+++ src/components/HeadlineCards.jsx
@@ -0,0 +1,42 @@
+import React  from 'react'
+import image1 from '../images/pexels1.jpg'
+
+
+const HeadlineCards = () => {
+    return (
+        <div className='max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6'>
+            {/* Card */}
+            <div className='rounded-xl relative'>
+                {/* Overlay */}
+                <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+                    <p className='font-bold text-2xl px-2 pt-4'>Sun's Out BOGO's</p>
+                    <p className='px-2'>Through 8/26</p>
+                    <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+                </div>
+                <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' src={image1} alt='/'/>
+            </div>
+
+            <div className='rounded-xl relative'>
+                {/* Overlay */}
+                <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+                    <p className='font-bold text-2xl px-2 pt-4'>Sun's Out BOGO's</p>
+                    <p className='px-2'>Through 8/26</p>
+                    <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+                </div>
+                <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' src={image1} alt='/'/>
+            </div>
+            <div className='rounded-xl relative'>
+                {/* Overlay */}
+                <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+                    <p className='font-bold text-2xl px-2 pt-4'>Sun's Out BOGO's</p>
+                    <p className='px-2'>Through 8/26</p>
+                    <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+                </div>
+                <img className='max-h-[160px] md:max-h-[200px] w-full object-cover rounded-xl' src={image1} alt='/'/>
+            </div>
+
+        </div>
+    )
+}
+
+export default HeadlineCards
